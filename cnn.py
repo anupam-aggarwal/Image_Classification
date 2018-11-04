dc5de6264ee1c84b8ad7a2730f742218b8927540
Anupam <anupamsanjeev11@gmail.com>
2018-11-04 14:04:12 +0530

    second Commit with img folder

--- cnn.py
+++ cnn.py
@@ -1,6 +1,6 @@
 #importing the keras libraries and packages
 from keras.models import Sequential
-from keras.layers import Convolution2D
+from keras.layers import Conv2D
 from keras.layers import MaxPooling2D
 from keras.layers import Flatten
 from keras.layers import Dense
@@ -9,7 +9,7 @@ from keras.layers import Dense
 classifier = Sequential()
 
 # Convolution Layer
-classifier.add(Convolutio2D(32,3,3,input_shape = (64,3,3), activation='relu'))
+classifier.add(Conv2D(32,(3,3),input_shape = (64,64,3), activation='relu'))
 
 #Pooling Layer
 classifier.add(MaxPooling2D(pool_size = (2,2)))
@@ -18,11 +18,11 @@ classifier.add(MaxPooling2D(pool_size = (2,2)))
 classifier.add(Flatten())
 
 #Fully Connected Layer
-classifier.add(Dense(output_dim = 128,activation = 'relu'))
-classifier.add(Dense(output_dim = 602,activation = 'softmax'))
+classifier.add(Dense(units = 128,activation = 'relu'))
+classifier.add(Dense(units = 1,activation = 'softmax'))
 
 #compiling the CNN
-classifier.compile(optimizer = 'adam', loss = '', metrics = ['accuracy'] )
+classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'] )			#need to think about loss function
 
 # Fitting CNN to images
 from keras.preprocessing.image import ImageDataGenerator
@@ -35,21 +35,21 @@ train_datagen = ImageDataGenerator(
 
 test_datagen = ImageDataGenerator(rescale=1./255)
 
-training_set = train_datagen.flow_from_directory(
-        'image/train',
+training_set = train_datagen.flow_from_directory(									#need to think about batch_size, class mode
+        'img',
         target_size=(64, 64),
         batch_size=32,
         class_mode='binary')
 
 test_set = test_datagen.flow_from_directory(
-        'image/validation',
+        'img',
         target_size=(64, 64),
         batch_size=32,
         class_mode='binary')
 
-model.fit_generator(
+classifier.fit_generator(															#need to tweak its params
         training_set,
-        steps_per_epoch=2000,
-        epochs=50,
+        steps_per_epoch=551,
+        epochs=25,
         validation_data=test_set,
-        validation_steps=800)
+        validation_steps=100)
