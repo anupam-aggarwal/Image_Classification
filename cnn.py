4406e6fb4df623a84b146cb7b409bc7ceb93723b
Anupam <anupamsanjeev11@gmail.com>
2018-11-15 14:03:15 +0530

    6th commit- Model with data as numpy arrays- Probably overfitted model

--- cnn.py
+++ cnn.py
@@ -4,14 +4,56 @@ from keras.layers import Conv2D
 from keras.layers import MaxPooling2D
 from keras.layers import Flatten
 from keras.layers import Dense
+from keras import optimizers
+#from PIL import Image
 import numpy as np
+#import cv2
 import os
+
+'''
+#code to read images as numpy arrays and save them in files
+images_x = []
+images_y = []
+for filename in os.listdir("path/to/images"):
+	images_x.append(cv2.imread(filename))
+	images_y.append(int(filename.split("_")[0]))
+data_X = np.asarray(images_x)
+data_Y = np.asarray(images_y)
+np.save("Xdata",data_X)
+np.save("Ydata",data_Y)
+'''
+'''
+for i in range(0,10):
+	print(data_Y[i])
+	Image.fromarray(data_X[i],'RGB').show()
+'''	
+data_X = np.load("Xdata.npy")
+data_Y = np.load("Ydata.npy")
+data_Y = np.eye(np.max(data_Y)+1)[data_Y]
 
 #initialising the CNN
 classifier = Sequential()
 
 # Convolution Layer
-classifier.add(Conv2D(32,(3,3),input_shape = (64,64,3), activation='relu'))
+classifier.add(Conv2D(32,(3,3),input_shape = (32,32,3), activation='relu',padding='same'))
+
+# Convolution Layer
+classifier.add(Conv2D(32,(3,3), activation='relu',padding='same'))
+
+# Convolution Layer
+classifier.add(Conv2D(32,(3,3),activation='relu',padding='same'))
+
+#Pooling Layer
+classifier.add(MaxPooling2D(pool_size = (2,2)))
+
+# Convolution Layer
+classifier.add(Conv2D(64,(3,3), activation='relu',padding='same'))
+
+# Convolution Layer
+classifier.add(Conv2D(64,(3,3), activation='relu',padding='same'))
+
+# Convolution Layer
+classifier.add(Conv2D(64,(3,3), activation='relu',padding='same'))
 
 #Pooling Layer
 classifier.add(MaxPooling2D(pool_size = (2,2)))
@@ -20,50 +62,25 @@ classifier.add(MaxPooling2D(pool_size = (2,2)))
 classifier.add(Flatten())
 
 #Fully Connected Layer
+classifier.add(Dense(units = 2048,activation = 'relu'))
 classifier.add(Dense(units = 1024,activation = 'relu'))
-#classifier.add(Dense(units = 1,activation = 'sigmoid'))
 classifier.add(Dense(units = 600,activation = 'softmax'))
+
 #compiling the CNN
 classifier.compile(optimizer = 'adam', loss = 'categorical_crossentropy', metrics = ['accuracy'] )			#need to think about loss function
 
-# Fitting CNN to images
-from keras.preprocessing.image import ImageDataGenerator
 
-train_datagen = ImageDataGenerator(
-        rescale=1./255,
-        shear_range=0.2,
-        zoom_range=0.2,
-        horizontal_flip=True)
 
-test_datagen = ImageDataGenerator(rescale=1./255)
+classifier.fit(x=data_X,y=data_Y,batch_size=32,epochs=125,validation_split=0.01)
 
-training_set = train_datagen.flow_from_directory(									#need to think about batch_size, class mode
-        'image',
-        target_size=(64, 64),
-        batch_size=32,
-        class_mode='categorical')
-
-test_set = test_datagen.flow_from_directory(
-        'image',
-        target_size=(64, 64),
-        batch_size=32,
-        class_mode='categorical')
-print(training_set.class_indices)
-
-classifier.fit_generator(															#need to tweak its params
-        training_set,
-        steps_per_epoch=512,
-        epochs=25,
-        validation_data=test_set,
-        validation_steps=100)
 
 #to save model to disk
 # serialize model to JSON
 model_json = classifier.to_json()
-with open("../Model/model.json", "w") as json_file:
+with open("model.json", "w") as json_file:
     json_file.write(model_json)
 # serialize weights to HDF5
-classifier.save_weights("../Model/model.h5")
+classifier.save_weights("model.h5")
 print("Saved model to disk")
 
 
